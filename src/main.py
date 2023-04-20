d964e127c01a88e5ab4de4574c75317806f1a108
Abdelrahman Sallam <atsallam@uci.edu>
2023-04-20 14:54:37 -0700

    Script will now sum up scores for each tag in the assignment

--- src/main.py
+++ src/main.py
@@ -9,9 +9,21 @@ if __name__ == "__main__":
     canvasFileList = os.listdir(CANVAS_FILE_PATH)
     #get a list of the file paths in the ../Gradescope/ directory
     gradescopeFileList = os.listdir(GRADESCOPE_FILE_PATH)
+    gradeScopeScores = {}
     for gradescopeFile in gradescopeFileList:
         csvFile = open(GRADESCOPE_FILE_PATH + gradescopeFile, 'r')
         csvReader = csv.DictReader(csvFile)
         for row in csvReader:
-            print(row['Email'])
+            if(row['Name'] == '' or row['Tags'] == None):
+                continue
+            if row['Email'] not in gradeScopeScores:
+                gradeScopeScores[row['Email']] = {}
+            for tag in row['Tags'].split(','):
+                if tag not in gradeScopeScores[row['Email']]:
+                    gradeScopeScores[row['Email']][tag] = float(row['Score'])
+                else:
+                    gradeScopeScores[row['Email']][tag] += float(row['Score'])
+    for studentScores in gradeScopeScores:
+        print(gradeScopeScores[studentScores])
+
         csvFile.close()
