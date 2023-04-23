b79231b5995f771cc55c0a0511bdffdded9b76f4
Abdelrahman Sallam <atsallam@uci.edu>
2023-04-24 00:53:22 +0100

    Script now correctly aggregates data and updates the csv files

--- src/main.py
+++ src/main.py
@@ -3,44 +3,58 @@ import csv
 
 CANVAS_FILE_PATH = "../Canvas/"
 GRADESCOPE_FILE_PATH = "../Gradescope/"
+OUTPUT_FILE_PATH = "../Output/"
+def getGradescopeScores(assignment):
+    gradeScopeScores = {}
+    for question in os.listdir(GRADESCOPE_FILE_PATH + assignment):
+        csvFile = open(GRADESCOPE_FILE_PATH + assignment + '/' + question, 'r')
+        csvReader = csv.DictReader(csvFile)
+        for row in csvReader:
+            if(row['Name'] == '' or row['Tags'] == None):
+                continue
+            for tag in row['Tags'].split(','):
+                if tag not in gradeScopeScores:
+                    gradeScopeScores[tag] = {}
+                if assignment not in gradeScopeScores[tag]:
+                    gradeScopeScores[tag][assignment] = {}
+                if row['Name'] not in gradeScopeScores[tag][assignment]:
+                    gradeScopeScores[tag][assignment][row['Name']] = float(row['Score'])
+                else:
+                    gradeScopeScores[tag][assignment][row['Name']] += float(row['Score'])
+        csvFile.close()
+    return gradeScopeScores
+
+def updateCanvasScores(gradeScopeScores):
+    for tag in gradeScopeScores:
+        try:
+            csvInput = open(CANVAS_FILE_PATH + "Rubric Scores " + tag + ".csv", 'r')
+        except:
+            print("Could not find the file: " + CANVAS_FILE_PATH + "Rubric Scores " + tag + ".csv")
+            continue
+        csvOutput = open(OUTPUT_FILE_PATH + "Updated Rubric Scores " + tag + ".csv", 'w', newline='')
+        csvReader = csv.DictReader(csvInput)
+        fieldnames = []
+        for field in csvReader.fieldnames:
+            fieldnames.append(field)
+        for assignment in gradeScopeScores[tag]:
+            fieldnames.append(assignment)
+        csvWriter = csv.DictWriter(csvOutput, fieldnames=fieldnames)
+        csvWriter.writeheader()
+        for assignment in gradeScopeScores[tag]:
+            for row in csvReader:
+                if row['Student Name'] in gradeScopeScores[tag][assignment]:
+                    print(row['Student Name'] + " " + tag + " " + assignment + " " + str(gradeScopeScores[tag][assignment][row['Student Name']]))
+                    row[assignment] = str(gradeScopeScores[tag][assignment][row['Student Name']])
+                csvWriter.writerow(row)
+        csvInput.close()
+        csvOutput.close()
 
 if __name__ == "__main__":
     #get a list of the file paths in the ../Canvas/ directory
     canvasFileList = os.listdir(CANVAS_FILE_PATH)
     #get a list of the file paths in the ../Gradescope/ directory
-    gradescopeFileList = os.listdir(GRADESCOPE_FILE_PATH)
-    gradeScopeScores = {}
-    for gradescopeFile in gradescopeFileList:
-        csvFile = open(GRADESCOPE_FILE_PATH + gradescopeFile, 'r')
-        csvReader = csv.DictReader(csvFile)
-        for row in csvReader:
-            if(row['Name'] == '' or row['Tags'] == None):
-                continue
-            if row['Name'] not in gradeScopeScores:
-                gradeScopeScores[row['Name']] = {}
-            if gradescopeFile not in gradeScopeScores[row['Name']]:
-                gradeScopeScores[row['Name']][gradescopeFile] = {}
-            for tag in row['Tags'].split(','):
-                if tag not in gradeScopeScores[row['Name']][gradescopeFile]:
-                    gradeScopeScores[row['Name']][gradescopeFile][tag] = float(row['Score'])
-                else:
-                    gradeScopeScores[row['Name']][gradescopeFile][tag] += float(row['Score'])
-        csvFile.close()
-
-    #for student in gradeScopeScores:
-        # for assignment in gradeScopeScores[student]:
-        #     for tag in gradeScopeScores[student][assignment]:
-        #         #open the Canvas file Rubric Scores [tag].csv
-        #         csvFile = open(CANVAS_FILE_PATH + "Rubric Scores " + tag + ".csv", 'r')
-        #         csvReader = csv.DictReader(csvFile)
-        #         #check if the assignment is one of the columns
-        #         if assignment not in csvReader.fieldnames:
-        #             #if not, add it
-        #             csvReader.fieldnames.insert(assignment)
-        #         #find the row with the student's name
-        #         for row in csvReader:
-        #             if row['Student'] == student:
-        #                 #add the score to the row
-        #                 row[assignment] = gradeScopeScores[student][assignment][tag]
-        #                 break
-
+    gradescopeAssignmentList = os.listdir(GRADESCOPE_FILE_PATH)
+    for assignment in gradescopeAssignmentList:
+        scores = getGradescopeScores(assignment)
+        print(scores)
+        updateCanvasScores(scores)
